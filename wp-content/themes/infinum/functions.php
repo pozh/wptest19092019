6d9cfdfeb11b0e7f5ba1b33da9257fdb59ba12f4
Sergey P <pozhilov@gmail.com>
2019-09-19 18:35:11 +0300

    Header - barely styled, WIP

--- wp-content/themes/infinum/functions.php
+++ wp-content/themes/infinum/functions.php
@@ -4,3 +4,13 @@
 add_action( 'wp_enqueue_scripts', function() {
 	wp_enqueue_style( 'main', get_stylesheet_directory_uri() . '/assets/styles/main.css' );
 });
+
+add_action( 'after_setup_theme', function () {
+	add_theme_support( 'title-tag' );       // No hard-coded <title> in the doc's head, WordPress handles it for us.
+	add_theme_support( 'post-thumbnails' ); // Enable Post Thumbnails on posts and pages.
+	register_nav_menus(
+		array(
+			'header-menu' => __( 'Main' ),
+		)
+	);
+} );
--- wp-content/themes/infinum/header.php
+++ wp-content/themes/infinum/header.php
@@ -7,3 +7,25 @@
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
+<header>
+
+	<?php
+	$ico_baseuri = get_template_directory_uri() . '/assets/images/icons/';
+	?>
+
+	<div class="container">
+		<div class="navbar">
+			<h1 class="navbar__logo"><a
+						href="<?= esc_url( home_url( '/' ) ); ?>"><span><?php bloginfo( 'name' ); ?></span></a></h1>
+			<div class="navbar__nav">
+				<?php wp_nav_menu( array( 'menu' => 'Main', 'container' => false, ) ); ?>
+			</div>
+			<div class="navbar__actions">
+				<ul>
+					<li><a class="btn btn-primary text-uppercase" href=""><img src="<?php echo $ico_baseuri . 'ic-apple.svg' ?>" alt=""> Get for iOS</a></li>
+					<li><a class="btn btn-secondary" href=""><img src="<?php echo $ico_baseuri . 'ic-unicorn.svg' ?>" alt=""> Unicorn owners</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</header>
