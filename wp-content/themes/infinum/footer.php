d9eb113e00375986ad71fe53ec90a576c1527bbe
Sergey P <pozhilov@gmail.com>
2019-09-19 16:44:26 +0300

    Footer php - initial commit

---
+++ wp-content/themes/infinum/footer.php
@@ -0,0 +1,19 @@
+<footer class="footer">
+	<div class="container">
+		<div class="footer-info">
+			<div class="footer-info__main">
+				<div class="footer__logo"></div>
+				<p>&copy; <?php _e( date( 'Y' ) ); ?> Uniduck. All rights reserved</p>
+			</div>
+			<div class="footer-info__actions">
+				<a href="">Like <span>Uniduck</span> on <i class="footer-icon__fb"></i></a>
+				<a href="">Follow <span>Uniduck</span> on <i class="footer-icon__twitter"></i></a>
+				<a href="">Follow <span>Uniduck</span> on <i class="footer-icon__instagram"></i></a>
+			</div>
+		</div>
+	</div>
+</footer>
+
+<?php wp_footer(); ?>
+</body>
+</html>
